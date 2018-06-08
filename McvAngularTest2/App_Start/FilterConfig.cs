485db4765df36143e3ac51c250071aa2ecb45f0b
Michał Kołodziejczyk <MKO@GLOBAL.PL>
2018-06-08 09:27:12 +0200

    Add project files.

---
+++ McvAngularTest2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace McvAngularTest2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
