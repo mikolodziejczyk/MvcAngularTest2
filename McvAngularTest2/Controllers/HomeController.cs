e349e5b854714513b56fa8635297b05ae00e2edc
mko <mko@GS-MKO-01>
2018-06-13 10:04:48 +0200

    metadata in a separate class

--- McvAngularTest2/Controllers/HomeController.cs
+++ McvAngularTest2/Controllers/HomeController.cs
@@ -44,56 +44,9 @@ namespace McvAngularTest2.Controllers
 
         public ActionResult FormMetadata()
         {
-            FormMetadata fd = new FormMetadata()
-            {
-                saveUrl = Url.RouteUrl("api", new { controller = "Home", action = "Save" }),
-                okUrl = Url.Action("Index"),
-                cancelUrl = Url.Action("About")
-            };
+            FormMetadata fd = MyFormMetadata.GetMetadata();
 
-            fd.controls.Add("unitPrice", new DecimalControlMetadata()
-            {
-                id = "unitPrice_id",
-                name = "unitPrice_name",
-                label = "Cena jednostkowa",
-                isRequired = false,
-                help = "Cena jednostkowa za towar bez uwzględnienia rabatów. Szczegóły <small><a href='http://global-solutions.pl'>Pomoc 21342</a></small>",
-                min = 0,
-                max = 100000,
-                maxDecimalDigits = 2
-            });
-
-            fd.controls.Add("startYear", new IntegerControlMetadata()
-            {
-                label = "Rok - początek",
-                isRequired = true,
-                help = "Rok początkowy <b>lorem ipsum</b> with html. <i>This is dynamic</i>",
-                placeholder = "Rok początkowy",
-                maxLength = 4,
-                controlSize = "medium",
-                min = 1900,
-                max = 2100
-            });
-
-            fd.controls.Add("lastName", new StringControlMetadata()
-            {
-                label = "Nazwisko",
-                isRequired = true,
-                controlSize = "medium",
-                maxLength = 20,
-                minLength = 2
-            });
-
-            fd.controls.Add("notifyViaMail", new CheckboxControlMetadata()
-            {
-                label = "Wyślij e-mail",
-                help = "Zaznacz aby otrzymywać powiadomienia poprzez e-mail.",
-                additionalLabel = "Powiadomienia e-mail"
-            });
-
-
-
-            var rs = JsonConvert.SerializeObject(fd, new JsonSerializerSettings
+             var rs = JsonConvert.SerializeObject(fd, new JsonSerializerSettings
             {
                 NullValueHandling = NullValueHandling.Ignore
             });
