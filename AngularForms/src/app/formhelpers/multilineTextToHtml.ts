d3c129f528ce6da344d71583c5c5c33287727548
mko <mko@GS-MKO-01>
2018-06-15 13:30:03 +0200

    FormGroup working

--- AngularForms/src/app/formhelpers/multilineTextToHtml.ts
+++ AngularForms/src/app/formhelpers/multilineTextToHtml.ts
@@ -4,7 +4,16 @@
  * @param text The text to replace crlf with <br/>
  */
 export function multilineTextToHtml(text: string): string {
-    let rx = /\r\n/g;
-    let r = text.replace(rx, "<br/>");
+    let r: string | null | undefined;
+
+    if (text) {
+
+        let rx = /\r\n/g;
+        r = text.replace(rx, "<br/>");
+    }
+    else {
+        r = text
+    }
+    
     return r;
 }
