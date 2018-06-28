d7eac3437f9070b45e56cd882e842cf36a061adb
mko <mko@GS-MKO-01>
2018-06-28 15:23:54 +0200

    dynamic columns working

--- primeNgTest/src/app/connection-index/connection-index.component.ts
+++ primeNgTest/src/app/connection-index/connection-index.component.ts
@@ -11,59 +11,42 @@ import { ConnectionVM } from '../connectionVM';
 })
 export class ConnectionIndexComponent implements OnInit {
 
-  constructor(private connectionListService: ConnectionListService) { }
+  constructor(private connectionListService: ConnectionListService) {
 
-  ngOnInit() {
-    this.loadConnections();
+    this.cols = [
+      { field: 'ppe', header: 'PPE', isSortable: true },
+      { field: 'meterCode', header: 'Kod licznika', isSortable: true },
+      { field: 'name', header: 'Nazwa', isSortable: true, isHtml: true },
+      { field: 'tariff', header: 'Taryfa', isSortable: true },
+      { field: 'company', header: 'Akronim', isSortable: false }
+    ];
   }
 
-  // allData: Connection[];
+  ngOnInit() {
+
+  }
+
+  cols: any[];
   connectionCount: number;
   connections: ConnectionVM[];
   loading: boolean = false;
 
-  async loadConnections() {
-
-    // try {
-    //   this.allData = await this.connectionListService.getConnections();
-    //   // this.connectionCount = this.allData.length;
-    //   console.log(`Successfully retrieved ${this.allData.length} connections.`);
-    // }
-    // catch (e) {
-    //   console.log(`Retrieving connections failed, ${e}`);
-    // }
-
-  }
 
   async loadData(event: LazyLoadEvent) {
-    //event.first = First row offset
-    //event.rows = Number of rows per page
-    //event.sortField = Field name to sort in single sort mode
-    //event.sortOrder = Sort order as number, 1 for asc and -1 for dec in single sort mode
-    //multiSortMeta: An array of SortMeta objects used in multiple columns sorting. Each SortMeta has field and order properties.
-    //filters: Filters object having field as key and filter value, filter matchMode as value
-    //globalFilter: Value of the global filter if available
-    // this.cars = //do a request to a remote datasource using a service and return the cars that match the lazy load criteria
-    
     console.log(`Update: ${JSON.stringify(event)}`);
 
     this.loading = true;
-    // window.setTimeout(() => {
-    //   this.connections = this.allData.slice(event.first, event.first + event.rows);
-    //   this.connectionCount = this.allData.length;
-    //   this.loading = false;
-    // }, 2000);
 
     try {
       let dataPage = await this.connectionListService.getConnectionPage(event);
       console.log(`Successfully retrieved ${dataPage.count} connections.`);
-      this.connections = dataPage.rows.map(x=> new ConnectionVM(x));
+      this.connections = dataPage.rows.map(x => new ConnectionVM(x));
       this.connectionCount = dataPage.count;
     }
-    catch(e) {
+    catch (e) {
       console.log(`Retrieving connections failed, ${e}`);
     }
 
     this.loading = false;
-}
+  }
 }
